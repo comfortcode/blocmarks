e3b47cf5daa2950a4c671a2decd2956cd31b58b2
comfortcode <nechamabgoldman@gmail.com>
2016-03-10 21:27:03 +0000

    incoming controller edit

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -8,7 +8,7 @@ class IncomingController < ApplicationController
      # to get a sense of what you're dealing with.
      puts "Sender: #{params[:sender]}"
      puts "subject: #{params[:subject]}"
-     puts "body: #{params[:body]}"
+     puts "body: #{params[:stripped_text]}"
      
      # You put the message-splitting and business
      # magic here.
