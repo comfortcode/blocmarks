a24aa64b205f5d20fbb14a68ca259245cec2f113
comfortcode <nechamabgoldman@gmail.com>
2016-03-11 04:52:49 +0000

    tweaked Incoming Controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,6 +1,5 @@
 class IncomingController < ApplicationController
 
-  # http://stackoverflow.com/questions/1177863/how-do-i-ignore-the-authenticity-token-for-specific-actions-in-rails
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
@@ -9,14 +8,11 @@ class IncomingController < ApplicationController
 
     if sender.present?
         
+        # create or find the topic...
         topic = Topic.create_with(user: sender).find_or_create_by(title: params[:subject])
-
-        # topic = sender.topics.find_by(title: params[:subject])
-    
-        # if topic.nil? # need to create new topic...
-        #     topic = sender.topics.new(title: params[:subject])
-        # end 
         
+        
+        # create and save the bookmark...
         new_bookmark = topic.bookmarks.new(url: params[:'stripped-text'])
     
         if new_bookmark.save
@@ -28,19 +24,6 @@ class IncomingController < ApplicationController
         puts "There was an error saving the bookmark as the sender #{params[:sender]} was not a found user. Subject: #{params[:subject]}, Body: #{params[:'stripped-text']}"
     end 
 
-     # You put the message-splitting and business
-     # magic here.
-     # Find the user by using params[:sender]
-     # Find the topic by using params[:subject]
-     # Assign the url to a variable after retreiving it from params["body-plain"]
-
-     # Check if user is nil, if so, create and save a new user
-
-     # Check if the topic is nil, if so, create and save a new topic
-
-     # Now that you're sure you have a valid user and topic, build and save a new bookmark
-
-    # Assuming all went well.
     head 200
   end
 end
