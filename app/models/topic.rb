883d69e3fc4be896bf7d3c16427f1078e5ce7cb7
comfortcode <nechamabgoldman@gmail.com>
2016-06-30 10:46:23 +0000

    moved method to model

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,14 @@
 class Topic < ActiveRecord::Base
   belongs_to :user
   has_many :bookmarks, :dependent => :destroy
+
+  def self.find_or_create_by_topic(*args)
+    options = args.extract_options!
+    options[:name] = args[0] if args[0].is_a?(String)
+    case_sensitive = options.delete(:case_sensitive)
+    conditions = case_sensitive ? ['name = ?', options[:name]] : 
+                                  ['UPPER(name) = ?', options[:name].upcase] 
+    first(:conditions => conditions) || create(options)
+  end
+
 end
